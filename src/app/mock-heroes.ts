96c3505c0eddc4946820a4e5d71054bdf1fb454b
koko <koko@users.noreply.github.com>
2019-01-05 01:10:30 +0200

    master detail view

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -5,10 +5,10 @@ export const HEROES: Hero[] = [
   { id: 2, name: 'Narco' },
   { id: 3, name: 'Bombasto' },
   { id: 4, name: 'Celeritas' },
-  { id: 5, name: 'Magneta' },
+  { id: 5, name: 'Sde' },
   { id: 6, name: 'RubberMan' },
   { id: 7, name: 'Dynama' },
   { id: 8, name: 'Dr IQ' },
-  { id: 9, name: 'Magma' },
+  { id: 9, name: 'Ads' },
   { id: 10, name: 'Tornado' }
 ];
