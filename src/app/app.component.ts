938e2cbfe8e258830a703251f853854af180e9e7
koko <koko@users.noreply.github.com>
2019-01-06 19:28:59 +0200

    add dashboard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'tests';
+    title = window.location.origin;
+
+    constructor() {
+    }
 }
